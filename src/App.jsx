391cdebd82a8194f18b5a1803c6e6e53927eba47
Prakash K <prakash.change@gmail.com>
2026-04-21 12:48:42 +0530

    link from react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import TestPage from "./pages/TestPage";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<Pricing />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <h1>React Router</h1>
+
+      <BrowserRouter>
+        <Link to={"/"} style={{ textDecoration: "none", marginRight: "10px" }}>
+          Home
+        </Link>
+        <Link
+          to={"/product"}
+          style={{ textDecoration: "none", marginRight: "10px" }}
+        >
+          Product
+        </Link>
+        <Link to={"/pricing"} style={{ textDecoration: "none" }}>
+          Pricing
+        </Link>{" "}
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="test" element={<TestPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/pages/TestPage.jsx
@@ -0,0 +1,5 @@
+function TestPage() {
+  return <div>Testing Router</div>;
+}
+
+export default TestPage;
