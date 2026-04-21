bda7b8d30dfab8fc2bbcd470e434f89681fd90da
Prakash K <prakash.change@gmail.com>
2026-04-21 13:35:25 +0530

    use navlink

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,20 @@ import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import TestPage from "./pages/TestPage";
+import PageNotFound from "./pages/PageNotFound";
+import "./index.css";
 function App() {
   return (
     <>
       <h1>React Router</h1>
 
       <BrowserRouter>
-        <Link to={"/"} style={{ textDecoration: "none", marginRight: "10px" }}>
-          Home
-        </Link>
-        <Link
-          to={"/product"}
-          style={{ textDecoration: "none", marginRight: "10px" }}
-        >
-          Product
-        </Link>
-        <Link to={"/pricing"} style={{ textDecoration: "none" }}>
-          Pricing
-        </Link>{" "}
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
           <Route path="test" element={<TestPage />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,13 @@
+import PageNav from "../components/PageNav";
+
 function Homepage() {
-  return <div>Worldwise</div>;
+  return (
+    <div>
+      <PageNav />
+
+      <h1>Worldwise</h1>
+    </div>
+  );
 }
 
 export default Homepage;
